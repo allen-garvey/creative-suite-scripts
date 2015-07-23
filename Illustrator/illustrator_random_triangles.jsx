a334b57f94c1c337d832481d128f4951165599b6
Allen Garvey <allengarvey@hotmail.com>
2015-07-23 11:12:20 -0400

    rename and refactor to use includes

--- Illustrator/illustrator_random_triangles.jsx
+++ Illustrator/illustrator_random_triangles.jsx
@@ -1,3 +1,4 @@
+#include lib/index.jsx
 /*
 * Creates a random triangles that fit to artboard
 * Sizing doesn't work correctly on new artboard for some reason
@@ -5,6 +6,7 @@
 //creates new document if not already open
 var doc = app.documents.length > 0 ? app.activeDocument : app.documents.add(DocumentColorSpace.RGB, 512, 512);
 var triangleAmount = 100;
+var color = new JSX.color.GreyscaleColor();
 
 function createRandomTriangle(){
 	var line = doc.pathItems.add();
@@ -16,24 +18,7 @@ function createRandomTriangle(){
 						[doc.width * Math.random(), doc.height * Math.random() * -1]
 						]);
 
-	line.fillColor = randGreyScaleColor();
-}
-
-function randGreyScaleColor(){
-	var value = Math.random() * 255;
-	var randColor = new RGBColor();
-	randColor.red = value;
-	randColor.green = value;
-	randColor.blue = value;
-	return randColor;
-}
-
-function randColor(){
-	var randColor = new RGBColor();
-	randColor.red = Math.random() * 255;
-	randColor.green = Math.random() * 255;
-	randColor.blue = Math.random() * 255;
-	return randColor;
+	line.fillColor = color.randColor();
 }
 
 for (var i = 0; i < triangleAmount; i++) {
