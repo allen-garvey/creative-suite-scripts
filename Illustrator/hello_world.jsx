990682f0ce67f1a2ff7bc299d9d449668c6d4e72
Allen Garvey <allengarvey@hotmail.com>
2015-07-24 20:16:17 -0400

    refactor to use includes and print in center of screen

--- Illustrator/hello_world.jsx
+++ Illustrator/hello_world.jsx
@@ -1,7 +1,10 @@
-//Hello World!
-var myDocument = app.documents.add();
-//Create a new text frame and assign it to the variable "myTextFrame" 
-var myTextFrame = myDocument.textFrames.add();
+#include lib/index.jsx
+/*
+* Writes hello world in a text box in the middle of the document
+*/
+
+//Creates new text frame
+var textFrame = JSX.doc.textFrames.add();
 // Set the contents and position of the text frame 
-myTextFrame.position = [200,200];
-myTextFrame.contents = "Hello World!";
+textFrame.position = [JSX.doc.width / 2,JSX.doc.height / 2 * -1];
+textFrame.contents = "Hello World!";
