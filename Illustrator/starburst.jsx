75ba94cdaf3282c41b3f4fa2c09b2cb612a025f9
Allen Garvey <allengarvey@hotmail.com>
2015-07-24 23:14:35 -0400

    add square and triangle pinwheel functions

--- Illustrator/starburst.jsx
+++ Illustrator/starburst.jsx
@@ -18,6 +18,44 @@ function createStarburst(radius, numPoints, radiusTransformFunc){
 	}
 }
 
+function createSquarePinwheel(radius, numPoints, squareWidth, radiusTransformFunc){
+	var centerPoint = JSX.vector.centerPoint();
+	var RADIANS_IN_CIRCLE = 2;
+	var arcRadLength = RADIANS_IN_CIRCLE / numPoints;
+	radiusTransformFunc = radiusTransformFunc ? radiusTransformFunc : function(radius, i){return radius;};
+	for (var i = 0; i < numPoints; i++) {
+		var tRadius = radiusTransformFunc(radius, i);
+		var radsFromOrigin = arcRadLength * i;
+		var circleApexPoint = parametricCirclePoint(centerPoint, tRadius, radsFromOrigin * Math.PI);
+		drawPath([centerPoint, 
+			circleApexPoint, 
+			parametricCirclePoint(circleApexPoint, squareWidth, (radsFromOrigin - .5) * Math.PI),
+			parametricCirclePoint(centerPoint, squareWidth, (radsFromOrigin - .5) * Math.PI)]);
+	}
+}
+
+function createTrianglePinwheel(radius, numPoints, squareWidth, radiusTransformFunc){
+	var centerPoint = JSX.vector.centerPoint();
+	var RADIANS_IN_CIRCLE = 2;
+	var arcRadLength = RADIANS_IN_CIRCLE / numPoints;
+	radiusTransformFunc = radiusTransformFunc ? radiusTransformFunc : function(radius, i){return radius;};
+	for (var i = 0; i < numPoints; i++) {
+		var tRadius = radiusTransformFunc(radius, i);
+		var radsFromOrigin = arcRadLength * i;
+		var circleApexPoint = parametricCirclePoint(centerPoint, tRadius, radsFromOrigin * Math.PI);
+		var widthPoint = parametricCirclePoint(centerPoint, squareWidth, (radsFromOrigin - .5) * Math.PI);
+		drawPath([centerPoint, 
+			circleApexPoint,
+			[JSX.math.random.randInt(widthPoint[0], circleApexPoint[0]), JSX.math.random.randInt(widthPoint[1], circleApexPoint[1])]
+			]);
+	}
+}
+
+
+/*
+* convenience function to calculate coordinates on a circle
+* returns single point [x, y] created by combining parametricCircleX and parametricCircleY functions
+*/
 function parametricCirclePoint(centerPoint, radius, radDegrees){
 	var xCoord = parametricCircleX(centerPoint[0], radius, radDegrees);
 	var yCoord = parametricCircleY(centerPoint[1], radius, radDegrees);
@@ -50,11 +88,18 @@ function drawLine(pointsArray){
 	line.closed = true;
 	line.setEntirePath(pointsArray);
 }
+function drawPath(pointsArray){
+	var line = JSX.doc.pathItems.add();
+	line.stroked = false;
+	line.fillColor = color.getColor();
+	line.closed = true;
+	line.setEntirePath(pointsArray);
+}
 
 /*
 * Main app
 */
-var color = new JSX.color.Color();
-createStarburst(400, 100, function(radius, i){return radius * Math.random();});
-
+var color = new JSX.color.Palette();
+// createStarburst(400, 100, function(radius, i){return radius * Math.random();});
+createTrianglePinwheel(600, 5, 20, function(radius, i){return radius * Math.random();});
 
