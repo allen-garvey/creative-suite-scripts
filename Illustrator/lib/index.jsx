ed52917d625915019901916021b01fba9d49f4d0
Allen Garvey <allengarvey@hotmail.com>
2015-07-26 23:42:03 -0400

    move init code into separate include

--- Illustrator/lib/index.jsx
+++ Illustrator/lib/index.jsx
@@ -2,11 +2,9 @@
 * Serves as an index to import all includes
 * should be imported instead of includes individually
 */
-var JSX = {};
-//creates new document if not already open
-//Sizing (width and height) doesn't work correctly on new document for some reason
-JSX.doc = app.documents.length > 0 ? app.activeDocument : app.documents.add(DocumentColorSpace.RGB, 512, 512);
 
+#include init.jsx
+#include util.jsx
 #include collection.jsx
 #include math.jsx
 #include color.jsx
