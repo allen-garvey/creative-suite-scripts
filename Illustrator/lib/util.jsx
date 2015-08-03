1c059b049f0b7fa91b80ce5a71fd185641e5086d
Allen Garvey <allengarvey@hotmail.com>
2015-08-03 01:03:35 -0400

    add function to test for hidden items

--- Illustrator/lib/util.jsx
+++ Illustrator/lib/util.jsx
@@ -10,3 +10,11 @@ JSX = JSX || {};
 JSX.isItemLocked = function(item){
 	return item.locked || item.layer.locked;
 }
+
+/*
+* Used to test if an item is editable i.e. hidden or not
+* items are objects such as pathItem or rasterItem
+*/
+JSX.isItemHidden = function(item){
+	return item.hidden || !item.layer.visible;
+}
